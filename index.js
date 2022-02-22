eb7de6cef8e779b18984174a8071ba314484fc52
danielm2313 <daniel.margit config --global user.email daniel.martinez.00@hotmail.com>
2022-02-22 10:26:26 -0600

    commit tree 6e9c697faae969466d50c56947f08b6f0d4d3d98

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.theLowerCase()
+}
+function logShout(string) {
+    "Hello!".toUpperCase();
+}
+function logWhisper(string) {
+    "Hello!".toLowerCase();
+}
